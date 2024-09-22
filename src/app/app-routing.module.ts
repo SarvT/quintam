0276a7b91932e3cc4312401918daf6590ab5ba9a
SarvT <91484197+SarvT@users.noreply.github.com>
2024-09-22 16:06:01 +0530

    Project initialization

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './src/app/Components/home/home.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '/home', component: HomeComponent },
+  { path: '/about', component: AppComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './src/app/Components/about/about.component';
+import { NavbarComponent } from './src/app/Components/navbar/navbar.component';
+import { HomeComponent } from './src/app/Components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
